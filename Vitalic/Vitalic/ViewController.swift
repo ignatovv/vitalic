eca6bbc37d1df10140121373a39b85a2a275f46b
Kazzman <kaiserr87@gmail.com>
2019-08-31 10:25:46 +0300

    Created new Xcode project

---
+++ Vitalic/Vitalic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vitalic
+//
+//  Created by Linar on 31/08/2019.
+//  Copyright © 2019 Linar Alimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
